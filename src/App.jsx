31a57c2175d6c57c9cf953aadf178577b4d75b6e
qberg <bs17b025@smail.iitm.ac.in>
2024-02-04 13:13:51 +0530

    template init with shadcn ui and tailwind

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <div>
+      <h1 className="text-4xl text-center pt-4">Hello from qberg</h1>
+      <Button>Click Me</Button>
+    </div>
+  );
+}
+
+export default App;
---
+++ vite.config.js
@@ -0,0 +1,17 @@
+import path from "path";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: [
+      { find: "@", replacement: path.resolve("./src/@/") },
+      {
+        find: "@/components",
+        replacement: path.resolve("./src/@/components/"),
+      },
+      { find: "@/lib", replacement: path.resolve("./src/@/lib/") },
+    ],
+  },
+});
