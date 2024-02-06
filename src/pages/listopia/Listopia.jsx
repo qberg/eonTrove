456d393c5cc47497c38d6e73595b07195733b3ed
codiisml <codiisml@pop-os.localdomain>
2024-02-06 12:20:57 +0530

    added route for books readlist

--- src/pages/listopia/Listopia.jsx
+++ src/pages/listopia/Listopia.jsx
@@ -1,12 +1,20 @@
 import React, { useEffect, useState } from "react";
 import Books from "../books/Books";
+import { Button } from "@/components/ui/button";
+import { Link } from "react-router-dom";
 
 const Listopia = () => {
   return (
     <div className="p-4 container mx-auto">
       <div className="flex flex-col justify-center items-center">
         <h1 className="font-leto text-2xl">Listopia</h1>
-        <div>Buttons for various lists goes here</div>
+        <div>
+          <Button 
+            className="mt-2"
+          >
+            <Link to="/books">Books</Link> 
+          </Button>
+        </div>
       </div>
     </div>
   );
